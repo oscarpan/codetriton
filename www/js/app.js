77035544204221b1684c41859f1796af2f297fcf
Oscar Pan <oscarpan888@gmail.com>
2016-04-23 10:16:47 -0700

    add ionic ratings

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'ngCordova', 'starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic', 'ngCordova', 'ionic-ratings', 'starter.controllers', 'starter.services'])
 
     .constant("config", {
       apiURL: "http://foodmash.oscarpan.com/api"
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -100,7 +100,31 @@ angular.module('starter.controllers', [])
         $scope.doRefresh = refreshData;
     })
 
-    .controller('OfferDetailCtrl', function($scope, $stateParams, $ionicPlatform, $cordovaFacebook) {
+    .controller('OfferDetailCtrl', function($scope, $stateParams, $localstorage, $filter, $ionicPlatform, $cordovaFacebook) {
+        var offerid = $stateParams.offerId;
+
+        var offers = $localstorage.getArray('offers');
+
+        $scope.offer = $filter('filter')(offers, {id: offerid})[0];
+
+        //console.log($scope.offer);
+
+        $scope.ratingsObject = {
+            iconOn: 'ion-ios-star',    //Optional
+            iconOff: 'ion-ios-star-outline',   //Optional
+            iconOnColor: 'rgb(200, 200, 100)',  //Optional
+            iconOffColor:  'rgb(200, 100, 100)',    //Optional
+            rating:  1, //Optional
+            minRating:1,    //Optional
+            readOnly: true, //Optional
+            callback: function(rating) {    //Mandatory
+                $scope.ratingsCallback(rating);
+            }
+        };
+
+        $scope.ratingsCallback = function(rating) {
+            console.log('Selected rating is : ', rating);
+        };
 
     })
 
