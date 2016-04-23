20bfbd93fa7a7cfcf3f18c1ebd81ccbf461c38c2
Oscar Pan <oscarpan888@gmail.com>
2016-04-23 09:58:02 -0700

    Added offers refresh functionality, reformat js to 4 spaces

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,96 +1,118 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope, $rootScope, $ionicPlatform, $cordovaFacebook, $localstorage, config, $http) {
+    .controller('DashCtrl', function($scope, $rootScope, $ionicPlatform, $cordovaFacebook, $localstorage, config, $http) {
 
-    $scope.localstorage = $localstorage;
+        $scope.localstorage = $localstorage;
 
-    $rootScope.user = $localstorage.getObject('user');
+        $rootScope.user = $localstorage.getObject('user');
 
-    $scope.form = {
-        f_b_user_id: $rootScope.user.id,
+        $scope.form = {
+            f_b_user_id: $rootScope.user.id,
 
-    };
+        };
 
-    $scope.host = function(form){
-        $http.post(config.apiURL+'/createOffer', form, config)
-            .then(function(success){
-                console.log(success);
-                $scope.form = {
-                    f_b_user_id: $rootScope.user.id,
+        $scope.host = function(form){
+            $http.post(config.apiURL+'/createOffer', form, config)
+                .then(function(success){
+                    console.log(success);
+                    $scope.form = {
+                        f_b_user_id: $rootScope.user.id,
 
-                };
-            });
-    };
+                    };
+                });
+        };
 
-    $ionicPlatform.ready(function() {
-        $cordovaFacebook.login(["public_profile", "email", "user_friends"])
-            .then(function (success) {
-                $scope.test3 = success;
+        $ionicPlatform.ready(function() {
+            $cordovaFacebook.getLoginStatus()
+                .then(function(success){
+                    console.log(success);
+                    if(success.status != "connected"){
 
-                $cordovaFacebook.api("me?fields=id,name,email", ["public_profile"])
-                    .then(function(success) {
 
-                        $scope.test = success;
+                        $cordovaFacebook.login(["public_profile", "email", "user_friends"])
+                            .then(function (success) {
+                                $scope.test3 = success;
 
-                        $localstorage.setObject('user', success);
-                        $rootScope.user = success;
+                                $cordovaFacebook.api("me?fields=id,name,email", ["public_profile"])
+                                    .then(function(success) {
 
-                        $scope.form.f_b_user_id = success.id;
+                                        $scope.test = success;
 
-                        $http.post(config.apiURL+'/userLogin', success, config)
-                            .then(function(success){
-                                $localstorage.setObject('user', success.data);
-                                $rootScope.user = success.data;
+                                        $localstorage.setObject('user', success);
+                                        $rootScope.user = success;
+
+                                        $scope.form.f_b_user_id = success.id;
+
+                                        $http.post(config.apiURL+'/userLogin', success, config)
+                                            .then(function(success){
+                                                $localstorage.setObject('user', success.data);
+                                                $rootScope.user = success.data;
+                                            });
+
+                                        console.log(success);
+                                    }, function (error) {
+                                        $scope.test = error;
+
+                                        console.log(error);
+                                    });
+
+                            }, function (error) {
+                                $scope.test = error;
                             });
 
-                        console.log(success);
-                    }, function (error) {
-                        $scope.test = error;
 
-                        console.log(error);
-                    });
+                    }
+                }, function(error){
+                    alert(error);
+                });
 
-            }, function (error) {
-                $scope.test = error;
-            });
+        });
 
+    })
+
+    .controller('OffersCtrl', function($scope, config, $http, $localstorage) {
+        // With the new view caching in Ionic, Controllers are only called
+        // when they are recreated or on app start, instead of every page change.
+        // To listen for when this page is active (for example, to refresh data),
+        // listen for the $ionicView.enter event:
+        //
+        $scope.$on('$ionicView.enter', function(e) {
+            refreshData();
+        });
+
+        $scope.offers = $localstorage.getArray('offers');
+
+        function refreshData(){
+            $http.get(config.apiURL+'/offers')
+                .then(function(data){
+                    console.log(data.data);
+                    $scope.offers = data.data;
+                    $localstorage.setObject('offers', data.data);
+                })
+                .finally(function(){
+                    $scope.$broadcast('scroll.refreshComplete');
+                });
+        }
+
+        refreshData();
+
+
+        $scope.doRefresh = refreshData;
+    })
+
+    .controller('OfferDetailCtrl', function($scope, $stateParams, $ionicPlatform, $cordovaFacebook) {
+
+    })
+
+    .controller('AccountCtrl', function($rootScope, $scope, $localstorage, $ionicPlatform, $cordovaFacebook) {
+
+        $rootScope.user = $localstorage.getObject('user');
+
+        //$ionicPlatform.ready(function() {
+        //    $cordovaFacebook.logout();
+        //});
+
+        $scope.settings = {
+            enableFriends: true
+        };
     });
-
-})
-
-.controller('ChatsCtrl', function($scope, config, $http, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-
-    $http.get(config.apiURL+'/offers').then(function(data){
-        console.log(data.data);
-        $scope.offers = data.data;
-    });
-})
-
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats, $ionicPlatform, $cordovaFacebook) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
-
-.controller('AccountCtrl', function($rootScope, $scope, $localstorage, $ionicPlatform, $cordovaFacebook) {
-
-    $rootScope.user = $localstorage.getObject('user');
-
-    $ionicPlatform.ready(function() {
-        $cordovaFacebook.logout();
-    });
-
-  $scope.settings = {
-    enableFriends: true
-  };
-});
